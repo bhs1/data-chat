c545bcbf3670cef697083242c9b3825ab2b071f3
Ben <bensc77@gmail.com>
2024-02-20 20:44:21 -0700

    Read imessage db to vector db. Still have dup prob

---
+++ code/imessage.py
@@ -0,0 +1,46 @@
+import sqlite3
+import pandas as pd
+
+########################################
+# Run this file to generate data/messages.txt
+########################################
+
+def get_imessages():
+    conn = sqlite3.connect('/Users/bensolis-cohen/Library/Messages/chat.db')
+    cur = conn.cursor()
+    cur.execute(" select name from sqlite_master where type = 'table' ")
+
+    messages = pd.read_sql_query('''SELECT ROWID, text, handle_id, date
+    FROM message T1 
+    INNER JOIN chat_message_join T2 
+        ON T2.chat_id=25
+        AND T1.ROWID=T2.message_id 
+    ORDER BY T1.date desc''', conn)
+    conn.close()
+    return messages
+
+HANDLE_DICTIONARY = {
+    0 : "Ben",
+    25 : "Scott",
+    19 : "Greg"
+}
+
+counters = {
+    "none_rows" : 0,
+    "total_rows": 0,
+}
+
+def load_database(messages_df):
+    with open('/Users/bensolis-cohen/Projects/Chat with my data/data/messages.txt', 'w') as file:
+        for index in range(len(messages_df) - 1, -1, -1):
+            row = messages_df.iloc[index]
+            raw_row = row['text']
+            counters["total_rows"] += 1
+            if raw_row is None:
+                counters["none_rows"] += 1
+                continue
+            file.write(f"{HANDLE_DICTIONARY[row['handle_id']]} says: {raw_row}\n")
+
+
+load_database(get_imessages())
+print(counters)
--- code/main.py
+++ code/main.py
@@ -3,27 +3,35 @@ import os
 import pickle
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import TextLoader
 from langchain_openai import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-# Load PDF
-loader = PyPDFLoader("/Users/bensolis-cohen/Projects/Chat with my data/data/iMessage Green Street Hooligans.pdf")
-pages_pickle_file = 'data/pages.pickle'
-if os.path.exists(pages_pickle_file):
-    with open(pages_pickle_file, 'rb') as f:
-        pages = pickle.load(f)
-else:
-    pages = loader.load()
-    with open(pages_pickle_file, 'wb') as f:
-        pickle.dump(pages, f)
+# Load messages
+loader = TextLoader("/Users/bensolis-cohen/Projects/Chat with my data/data/messages.txt")
+pages = loader.load()
 
 # Split documents
 # TODO(bensc): Fix problem where we have duplicate documents over multiple pages.
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-splits = text_splitter.split_documents(pages)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=100)
+splits_raw = text_splitter.split_documents(pages)
+
+splits = []
+seen_texts = set()
+i = 0
+
+counters = {
+    "num_non_dups" : 0,
+    "total_docs" : 0,
+}
+for split in splits_raw:
+    counters["total_docs"] += 1
+    if split.page_content not in seen_texts:
+        counters["num_non_dups"] += 1
+        seen_texts.add(split.page_content)
+        splits.append(split)
 
 # Set up OpenAI and Chroma
 openai.api_key = os.environ['OPENAI_API_KEY']
@@ -42,22 +50,25 @@ vectordb = Chroma(
     embedding_function=embedding
 )
 
+
 # Build prompt
-template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer. 
+template = """Use the following pieces of context to answer the question at the end. Try to be a bit funny but make sure you convey all the information you can find.
 {context}
 Question: {question}
-Helpful Answer:"""
+Informative Answer based on the chat conversation above:"""
 QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
 
 llm_name = "gpt-3.5-turbo"
 llm = ChatOpenAI(model_name=llm_name, temperature=0)
 qa_chain = RetrievalQA.from_chain_type(
     llm,
-    retriever=vectordb.as_retriever(search_kwargs={"k": 10}),
+    retriever=vectordb.as_retriever(search_kwargs={"k": 7}),
     return_source_documents=True,
  #   chain_type="refine"#,
     chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
 )
+
+print(counters)
 
 while True:
     question = input("Please ask a question then press [enter]: ")
